57196888bc62e54d5b6fe3c7f2626545b5e515ba
samarth2434 <140902962+samarth2434@users.noreply.github.com>
2023-08-28 22:56:48 +0530

    main

--- lecture 1/main.cpp
+++ lecture 1/main.cpp
@@ -1,7 +1,35 @@
 #include<iostream>
 using namespace std;
 
+
 int main(){
-    cout<<"Hello world"<<endl;
+
+    int a =5;
+    cout << sizeof(a) << endl;
+
+    // int a =5;
+    // cout<<"VAlue of a is :"<< a <<endl; 
+
+    // char ch = 255;
+    // cout << "Value of ch is : "<< ch <<endl;
+
+    // bool flag = true;
+    // cout <<"Value of flag is" << flag << endl; 
+
+
+
+    // cout<<"Hello world"<<endl;
+    // // cout<<'\n';
+
+    // cout<<"Enter a number:"<<endl;
+
+    //    int a;
+    //    //Taking input
+    //    cin>>a;
+
+    //    cout<<"YOu entered "<< a <<endl;
+
+
+
 }
 
