881aa664d1bdd504c5940197d171c23ac96f0249
samarth2434 <140902962+samarth2434@users.noreply.github.com>
2023-08-29 23:31:34 +0530

    main

--- lecture 1/main.cpp
+++ lecture 1/main.cpp
@@ -5,7 +5,36 @@ using namespace std;
 int main(){
 
     int a =5;
-    cout << sizeof(a) << endl;
+    int b =3;
+
+    cout<< (a>=5 && b<=3);
+
+    cout<<!(a==5);
+    
+
+    // int a = 5;
+    // int b = 3;
+
+    // cout << a+b << endl;
+    // cout << a-b << endl;
+    // cout << a*b << endl;
+    // cout << a/b << endl;
+    // cout << a%b << endl;
+
+    // char ch = 1024;
+    // cout<< ch <<endl;
+
+
+    // double d = 5.7;
+
+    // int x = (int)d+2;
+    // cout<<"value of x is"<<x<<endl;
+
+    // char ch=97;
+    // cout<<ch<<endl;
+
+    // int a =5;
+    // cout << sizeof(a) << endl;
 
     // int a =5;
     // cout<<"VAlue of a is :"<< a <<endl; 
