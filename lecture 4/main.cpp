3cd170f4063c7569693bc3fdcd6ab9d6684d4e6f
samarth2434 <140902962+samarth2434@users.noreply.github.com>
2023-10-06 22:12:55 +0530

    main

--- lecture 4/main.cpp
+++ lecture 4/main.cpp
@@ -7,8 +7,8 @@ int main()
     cin >> n;
 
     // outer loop
-    for (int row = 0; row < n; row = row + 1)
-    {
+    for (int row = 0; row < n; row = row + 1){
+        
         // inner loop
         // Space print karege
         for (int col = 0; col < n - row - 1; col = col + 1)
