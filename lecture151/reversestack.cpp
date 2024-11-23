1337ad3fc046ff605f160f44bf04000acdb2fff6
samarth2434 <140902962+samarth2434@users.noreply.github.com>
2024-11-24 02:31:50 +0530

    main

--- lecture151/reversestack.cpp
+++ lecture151/reversestack.cpp
@@ -2,41 +2,48 @@
 #include<stack>
 using namespace std;
 
-void solve(stack<int> &s, int target){
+void insertAtBottom(stack<int> &s, int target){
     //base case 
-    if(s.empty()){
+    if(!s.empty()){
         s.push(target);
         return;
     }
+    
     int topElement = s.top();
     s.pop();
-    //rec call 
-    solve(s, target);
-    //Backtracking 
+    // rec call
+    insertAtBottom(s, target);
+    //BT
     s.push(topElement);
-} 
+}
 
-void insertAtBottom(stack<int> &s){
+void reverseStack(stack<int> &s){
+    //base case
     if(s.empty()){
-        cout<<"stack is empty, cant insert at bottom" << endl;
         return;
     }
-
+    
+    //find target 
     int target = s.top();
     s.pop();
-    solve(s, target); 
 
+    //reverse stack
+    reverseStack(s);
+    //insert at bottom target ko
+    insertAtBottom(s, target);
 }
 
 int main(){
-    stack<int> s;  // creation 
+
+    //create an stack 
+    stack<int> s;
     s.push(10);
     s.push(20);
     s.push(30);
     s.push(40);
     s.push(50);
 
-    insertAtBottom(s);
+    reverseStack(s);
 
     cout<< "Printing" << endl; 
     while(!s.empty()){
@@ -46,5 +53,4 @@ int main(){
     cout << endl;
 
     return 0; 
-
 }
