18e4a4b71e8b96c92de4c7832ac4ad19ed659fc9
samarth2434 <140902962+samarth2434@users.noreply.github.com>
2024-12-05 00:01:53 +0530

    main

--- lecture166/reversequeue.cpp
+++ lecture166/reversequeue.cpp
@@ -83,23 +83,67 @@ void reversek(queue<int> &q, int k){
 }
 }
 
+//question 3 - interleavequeue 
+void interleaveQueue(queue<int> &q){
+    //step 1
+    int n = q.size();
+    int k = n/2;
+    int count = 0;
+    queue<int> q2;
+
+    while(!q.empty()){
+        int temp = q.front();
+        q.pop();
+        q2.push(temp);
+        count++;
+
+        if(count == k)
+        break;
+    }
+
+    // step 2 
+    while(!q.empty() && !q2.empty()){
+        int first = q2.front();
+        q2.pop();
+
+        q.push(first);
+
+        int second = q.front();
+        q.pop();
+
+        q.push(second);
+    }
+    //odd wala case
+    if(n&1){
+        int element = q.front();
+        q.pop();
+        q.push(element);
+    }
+}
+
 
 int main(){
     queue<int> q;
-    q.push(3);
-    q.push(6);
-    q.push(9);
+    q.push(1);
     q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(6);
+    q.push(7);
     q.push(8);
+    // q.push(9);
 
     
     //method 1 
     // reverseQueue(q);
     //method 2
-    reverseQueueRecursion(q);
+    // reverseQueueRecursion(q);
 
     // quesion 2 
-    reversek(q,3);
+    // reversek(q,3);
+
+    //question 3
+    interleaveQueue(q);
     
     cout<<"Printing Queue"<<endl;
     while(!q.empty()){
